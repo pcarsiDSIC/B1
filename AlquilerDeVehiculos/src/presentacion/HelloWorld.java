0d7490aca428400b9e1ea201f5c912055767c0da
pcarsi <pcarsi@gmail.com>
2020-02-16 10:40:18 +0100

    Mejor lo dejo como estaba con el atributo

--- AlquilerDeVehiculos/src/presentacion/HelloWorld.java
+++ AlquilerDeVehiculos/src/presentacion/HelloWorld.java
@@ -2,9 +2,12 @@ package presentacion;
 
 public class HelloWorld {
 
+	// De momento no vale para nada
+	int a;
+	
 	// Arranque del HelloWorld
 	public static void main(String[] args) {
-		System.out.println("Hola Mon! Hello world!");
+		System.out.println("No hay conflicto. Todo arreglado.");
 	}
 
 }
