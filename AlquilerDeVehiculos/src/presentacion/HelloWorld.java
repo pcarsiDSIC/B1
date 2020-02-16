623dd4692865cb511d672eb48b3a54a2759de7c9
pcarsi <pcarsi@gmail.com>
2020-02-16 10:36:12 +0100

    Vuelta a la revisión de la fusión de idiomas

--- AlquilerDeVehiculos/src/presentacion/HelloWorld.java
+++ AlquilerDeVehiculos/src/presentacion/HelloWorld.java
@@ -2,12 +2,9 @@ package presentacion;
 
 public class HelloWorld {
 
-	// De momento no vale para nada
-	int a;
-	
 	// Arranque del HelloWorld
 	public static void main(String[] args) {
-		System.out.println("No hay conflicto. Todo arreglado.");
+		System.out.println("Hola Mon! Hello world!");
 	}
 
 }
