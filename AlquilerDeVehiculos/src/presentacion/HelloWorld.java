f449dcef665de50313da013eb571107aa076e26d
pcarsi <pcarsi@gmail.com>
2020-02-16 09:53:06 +0100

    Definido un nuevo atributo y añadida su descripción

--- AlquilerDeVehiculos/src/presentacion/HelloWorld.java
+++ AlquilerDeVehiculos/src/presentacion/HelloWorld.java
@@ -2,6 +2,9 @@ package presentacion;
 
 public class HelloWorld {
 
+	// De momento no vale para nada
+	int a;
+	
 	// Arranque del HelloWorld
 	public static void main(String[] args) {
 		System.out.println("No hay conflicto. Todo arreglado.");
