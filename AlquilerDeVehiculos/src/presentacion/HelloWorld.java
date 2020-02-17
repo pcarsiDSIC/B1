32916a02c4e2f8cb186c165ea7b01121e3a0bd74
pcarsDSIC <pcarsi@dsic.upv.es>
2020-02-17 16:25:12 +0100

    Cambiado el saludo inicial

--- AlquilerDeVehiculos/src/presentacion/HelloWorld.java
+++ AlquilerDeVehiculos/src/presentacion/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	// Arranque del HelloWorld
 	public static void main(String[] args) {
-		System.out.println("No hay conflicto. Todo arreglado.");
+		System.out.println("Bienvenidos");
 	}
 
 }
