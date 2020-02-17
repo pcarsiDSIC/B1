eda61d52058ead3e904dfdceff0a76c6af149fc5
pcarsDSIC <pcarsi@dsic.upv.es>
2020-02-17 16:16:23 +0100

    Saludo ampliado

--- AlquilerDeVehiculos/src/presentacion/HelloWorld.java
+++ AlquilerDeVehiculos/src/presentacion/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 	// Arranque del HelloWorld
 	public static void main(String[] args) {
 		System.out.println("No hay conflicto. Todo arreglado.");
+		másSaludo();
 	}
 
+	public static void másSaludo() {
+		System.out.println("   a todo el que lo quiera leer.");
+	}
 }
